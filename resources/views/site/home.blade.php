14edc09c8a87d36a84eade842889cfb819776cc6
João FIlipi Britto <jf.britto@hotmail.com>
2020-09-08 23:34:48 -0300

    melhorias

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -24,17 +24,17 @@
 <!-- Portfolio Section-->
 <section class="page-section portfolio" id="portfolio">
     <div class="container">
-        <!-- Portfolio Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
-        <!-- Icon Divider-->
+
+        <!-- <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+
         <div class="divider-custom">
             <div class="divider-custom-line"></div>
             <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
             <div class="divider-custom-line"></div>
         </div>
-        <!-- Portfolio Grid Items-->
+
         <div class="row">
-            <!-- Portfolio Item 1-->
+            
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -43,7 +43,7 @@
                     <img class="img-fluid" src="/template/assets/img/portfolio/cabin.png" alt="" />
                 </div>
             </div>
-            <!-- Portfolio Item 2-->
+            
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -52,7 +52,7 @@
                     <img class="img-fluid" src="/template/assets/img/portfolio/cake.png" alt="" />
                 </div>
             </div>
-            <!-- Portfolio Item 3-->
+            
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal3">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -61,7 +61,7 @@
                     <img class="img-fluid" src="/template/assets/img/portfolio/circus.png" alt="" />
                 </div>
             </div>
-            <!-- Portfolio Item 4-->
+            
             <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal4">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -70,7 +70,7 @@
                     <img class="img-fluid" src="/template/assets/img/portfolio/game.png" alt="" />
                 </div>
             </div>
-            <!-- Portfolio Item 5-->
+            
             <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal5">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -79,7 +79,7 @@
                     <img class="img-fluid" src="/template/assets/img/portfolio/safe.png" alt="" />
                 </div>
             </div>
-            <!-- Portfolio Item 6-->
+            
             <div class="col-md-6 col-lg-4">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal6">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -88,14 +88,14 @@
                     <img class="img-fluid" src="/template/assets/img/portfolio/submarine.png" alt="" />
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
 <!-- About Section-->
 <section class="page-section bg-primary text-white mb-0" id="about">
     <div class="container">
         <!-- About Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+        <h2 class="page-section-heading text-center text-uppercase text-white">Sobre</h2>
         <!-- Icon Divider-->
         <div class="divider-custom divider-light">
             <div class="divider-custom-line"></div>
@@ -104,33 +104,27 @@
         </div>
         <!-- About Section Content-->
         <div class="row">
-            <div class="col-lg-4 ml-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-            <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+            <div class="col-lg-12 text center"><p class="lead">Compartilhe todas suas redes em apenas um lugar.</p></div>
+            <!-- <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div> -->
         </div>
-        <!-- About Section Button-->
-        <div class="text-center mt-4">
-            <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/themes/freelancer/">
-                <i class="fas fa-download mr-2"></i>
-                Free Download!
-            </a>
-        </div>
+
     </div>
 </section>
 <!-- Contact Section-->
 <section class="page-section" id="contact">
     <div class="container">
-        <!-- Contact Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-        <!-- Icon Divider-->
+
+        <!-- <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+
         <div class="divider-custom">
             <div class="divider-custom-line"></div>
             <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
             <div class="divider-custom-line"></div>
         </div>
-        <!-- Contact Section Form-->
+
         <div class="row">
             <div class="col-lg-8 mx-auto">
-                <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
+
                 <form id="contactForm" name="sentMessage" novalidate="novalidate">
                     <div class="control-group">
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
@@ -165,46 +159,11 @@
                     <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Send</button></div>
                 </form>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
-<!-- Footer-->
-<footer class="footer text-center">
-    <div class="container">
-        <div class="row">
-            <!-- Footer Location-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Location</h4>
-                <p class="lead mb-0">
-                    2215 John Daniel Drive
-                    <br />
-                    Clark, MO 65243
-                </p>
-            </div>
-            <!-- Footer Social Icons-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Around the Web</h4>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
-            </div>
-            <!-- Footer About Text-->
-            <div class="col-lg-4">
-                <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                <p class="lead mb-0">
-                    Freelance is a free to use, MIT licensed Bootstrap theme created by
-                    <a href="http://startbootstrap.com">Start Bootstrap</a>
-                    .
-                </p>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- Copyright Section-->
-<div class="copyright py-4 text-center text-white">
-    <div class="container"><small>Copyright © Your Website 2020</small></div>
-</div>
+
+
 <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
 <div class="scroll-to-top d-lg-none position-fixed">
     <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
